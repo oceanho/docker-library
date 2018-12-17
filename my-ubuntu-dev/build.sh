290a148afb254a4a15f6d083977c70a2596a3285
hehai <hehai@arxanfintech.com>
2018-12-17 21:21:21 +0800

    Add my-ubuntu-dev

---
+++ my-ubuntu-dev/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+
+work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock --privileged -v ${work_dir}:/apps --workdir /apps --rm -it docker docker build -t my-dev-ubuntu:16.04 -f Dockerfile .
