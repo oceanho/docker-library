b2c3e6cf69761d4b0f2089ac7ba08a0c7c7a3241
hehai <hehai@arxanfintech.com>
2018-12-29 15:48:56 +0800

    redirect dir

--- my-ubuntu-dev/ubuntu1604/build.sh
+++ my-ubuntu-dev/ubuntu1604/build.sh
@@ -2,5 +2,4 @@
 #
 
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 docker run -v /var/run/docker.sock:/var/run/docker.sock --privileged -v ${work_dir}:/apps --workdir /apps --rm -it docker docker build -t my-dev-ubuntu:16.04 -f Dockerfile .
