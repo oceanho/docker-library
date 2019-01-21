329ee1c2d9e48d81d665dc019def2b9ff81dba77
hehai <hehai@arxanfintech.com>
2019-01-21 14:37:23 +0800

    update build.sh

--- my-ansible-dev/centos7/build.sh
+++ my-ansible-dev/centos7/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
-ld_pwd=$(pwd)
+old_pwd=$(pwd)
+target="my-dev-ansible:centos7"
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $work_dir
-docker build -t  my-dev-ansible:centos7 -f Dockerfile .
+docker build -t $target -f Dockerfile .
 cd $old_pwd
+echo "Done. docker image tag: $target"
--- my-ubuntu-dev/ubuntu1604/build.sh
+++ my-ubuntu-dev/ubuntu1604/build.sh
@@ -2,8 +2,10 @@
 #
 
 old_pwd=$(pwd)
+target="my-dev-ubuntu:16.04"
 work_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $work_dir
-docker build -t my-dev-ubuntu:16.04 -f Dockerfile .
+docker build -t $target -f Dockerfile .
 cd $old_pwd
+echo "Done. docker image tag: $target"
 
